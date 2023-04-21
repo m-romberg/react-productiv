49b233306e09d991f4dcd85cea48c88b5e742e69
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-21 12:26:24 -0700

    start writing test for EditableTodo

---
+++ src/EditableTodo.test.js
@@ -0,0 +1,30 @@
+import { render, fireEvent } from "@testing-library/react";
+import EditableTodo from "./EditableTodo";
+
+const todo = {
+  id: 1,
+  title: "Todo Title",
+  description: "Todo description",
+  priority: 1,
+};
+describe("Todo Tests", function () {
+  test("it renders without crashing", function () {
+    render(<Todo todo={todo} />);
+  });
+
+  test("it shows the todo contents with valid data", function () {
+    const todoRendered = render(<Todo todo={todo} />);
+    expect(todoRendered.queryByText(todo.title)).toBeInTheDocument();
+    expect(todoRendered.queryByText(todo.description)).toBeInTheDocument();
+    expect(todoRendered.queryByText(`(priority: ${todo.priority})`)).toBeInTheDocument();
+
+  });
+
+  test("it renders empty Todo and priority if no data passed", function (){
+    const todoRendered = render(<Todo />);
+    expect(todoRendered.queryByText(todo.title)).not.toBeInTheDocument();
+    expect(todoRendered.queryByText(todo.description)).not.toBeInTheDocument();
+    expect(todoRendered.queryByText(`(priority: )`)).toBeInTheDocument();
+  })
+}
+);
